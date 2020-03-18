7aee3928fd8b4e9ddc6d3946407a9817f419117b
unknown <bulavskipavel@gmail.com>
2020-03-18 09:51:33 +0300

    feat: add slider function

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ window.onload = function() {
 
   // Tags
   addTagsClickHandler();
+  addSliderClickHandler();
+  addSliderBackClickHandler();
 }
 
 const addTagsClickHandler = () => {
@@ -25,4 +27,36 @@ const removeSelectedTags = () => {
 const selectClickedTag = (clickedTag) => {
   clickedTag.classList.add('navigation_active');
   clickedTag.classList.remove('navigation_unactive');
+}
+
+// slider
+
+const addSliderClickHandler = () => {
+  document.querySelector('.wrapper_phones').addEventListener('click', (e) => {
+    if (e.target.classList.contains('arrow')) {
+      switchSlider();
+    }
+  })
+}
+
+const switchSlider = () => {
+  let slider = document.querySelector('.phones');
+  slider.style.display = 'none';
+  let nextSlider = document.querySelector('.phones_2');
+  nextSlider.style.display = 'block';
+}
+
+const addSliderBackClickHandler = () => {
+  document.querySelector('.wrapper_phones').addEventListener('click', (e) => {
+    if (e.target.classList.contains('arrow_2')) {
+      returnSlider();
+    }
+  })
+}
+
+const returnSlider = () => {
+  let slider_2 = document.querySelector('.phones_2');
+  slider_2.style.display = 'none';
+  let previousSlider = document.querySelector('.phones');
+  previousSlider.style.display = 'flex';
 }
